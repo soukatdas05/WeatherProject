6ee5d7b4e7425e03578abdae3655a444a4f7c0a3
bhaskar966 <deybhaskar999@gmail.com>
2024-03-15 09:32:57 +0530

    adding weather

--- weatherProject/urls.py
+++ weatherProject/urls.py
@@ -1,8 +1,7 @@
-"""
-URL configuration for weatherProject project.
+"""weatherproject URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.0/topics/http/urls/
+    https://docs.djangoproject.com/en/3.1/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -14,12 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path
-from weatherProject.views import *
 
+from django.urls import path ,include
+from django.conf import settings
+from django.conf.urls.static import static
+from weatherproject.views import *
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path("about/",about)
-]
+    path("about/",about),
+    path('', include('weatherapp.urls') ),
+] + static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
